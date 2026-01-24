80822e4edd10d57c7f3d94ae05376c3d1a4de928
Eva <eva.boekhold@gmail.com>
2026-01-24 23:06:20 +0100

    Separate concerns in QuotesController

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,35 +1,40 @@
 class QuotesController < ApplicationController
-  before_action :set_httpx
-
   # GET /quotes/{:id}
   def show
     quote = Quote.find(params.expect(:id))
 
-    process_and_render(quote)
+    remote_json = remote_quote(quote)
+
+    render json: prepare_json(remote_json, quote)
   end
 
   # GET /quotes/random
   def random
     quote = Quote.find(Quote.pluck(:id).sample)
 
-    process_and_render(quote)
+    remote_json = remote_quote(quote)
+
+    render json: prepare_json(remote_json, quote)
   end
 
   private
-    def process_and_render(quote)
-      response = @http.get(quote.remote_uri)
-      json = JSON.parse(response.body)
+    def remote_quote(quote)
+      http = HTTPX.with(headers: { 'accept': 'application/json', 'Content-Type': 'application/json' })
 
+      response = http.get(quote.remote_uri)
+
+      return JSON.parse(response.body)
+    end
+
+    def prepare_json(remote_json, quote)
+      json = remote_json
+      
       json["id"] = quote.id
       json["remote_uri"] = quote.remote_uri
 
       similar = quote.nearest_neighbors(:embedding, distance: "cosine").first(5)
-      json["similar_quotes"] = similar.map { |q| url_for(q) }
+      json["similar_quotes"] = similar.map { |similar_quote| url_for(similar_quote) }
 
-      render json: json
-    end
-
-    def set_httpx
-      @http = HTTPX.with(headers: { 'accept': 'application/json', 'Content-Type': 'application/json' })
+      return json
     end
 end
