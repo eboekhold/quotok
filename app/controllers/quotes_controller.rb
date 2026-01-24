0702ff0be7f98e46cff35d1ce4c2953ee675bab7
Eva <eva.boekhold@gmail.com>
2026-01-24 22:51:53 +0100

    DRY QuotesController

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,39 +1,35 @@
 class QuotesController < ApplicationController
+  before_action :set_httpx
+
   # GET /quotes/{:id}
   def show
-    http = HTTPX.with(headers: { 'accept': 'application/json', 'Content-Type': 'application/json' })
+    quote = Quote.find(params.expect(:id))
 
-    @quote = Quote.find(params.expect(:id))
-
-    response = http.get(@quote.remote_uri)
-    json = JSON.parse(response.body)
-
-    json["id"] = @quote.id
-    json["remote_uri"] = @quote.remote_uri
-
-    similar = @quote.nearest_neighbors(:embedding, distance: "cosine").first(5)
-    json["similar_quotes"] = similar.map { |q| url_for(q) }
-
-    render json: json
+    process_and_render(quote)
   end
 
   # GET /quotes/random
   def random
-    http = HTTPX.with(headers: { 'accept': 'application/json', 'Content-Type': 'application/json' })
+    quote = Quote.find(Quote.pluck(:id).sample)
 
-    @quote = Quote.find(Quote.pluck(:id).sample)
-
-    response = http.get(@quote.remote_uri)
-    json = JSON.parse(response.body)
-
-    @remote_json = JSON.parse(response.body)
-
-    json["id"] = @quote.id
-    json["remote_uri"] = @quote.remote_uri
-
-    similar = @quote.nearest_neighbors(:embedding, distance: "cosine").first(5)
-    json["similar_quotes"] = similar.map { |q| url_for(q) }
-
-    render json: json
+    process_and_render(quote)
   end
+
+  private
+    def process_and_render(quote)
+      response = @http.get(quote.remote_uri)
+      json = JSON.parse(response.body)
+
+      json["id"] = quote.id
+      json["remote_uri"] = quote.remote_uri
+
+      similar = quote.nearest_neighbors(:embedding, distance: "cosine").first(5)
+      json["similar_quotes"] = similar.map { |q| url_for(q) }
+
+      render json: json
+    end
+
+    def set_httpx
+      @http = HTTPX.with(headers: { 'accept': 'application/json', 'Content-Type': 'application/json' })
+    end
 end
