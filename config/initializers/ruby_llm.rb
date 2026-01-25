b0c687529d56b51a0aa514dfec54b9b6df3218b2
Eva <eva.boekhold@gmail.com>
2026-01-26 00:28:26 +0100

    Ensure OpenAI never gets called in test environment

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,7 @@
 require "ruby_llm"
 
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV.fetch("OPENAI_API_KEY", nil) || Rails.application.credentials.openai_api_key
+  if Rails.env.development? || Rails.env.production?
+    config.openai_api_key = ENV.fetch("OPENAI_API_KEY", nil) || Rails.application.credentials.openai_api_key
+  end
 end
